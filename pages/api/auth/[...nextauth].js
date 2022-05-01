a28601cf1e2f13e3e14eb487fe62eb1923801b46
INF-Zenyth <samuel.ward@teaminfernal.fr>
2022-05-01 22:54:03 +0200

    Discord + Twitter providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import TwitterProvider from "next-auth/providers/twitter";
 
 const options = {
 	providers: [
@@ -7,7 +9,19 @@ const options = {
 			clientId: process.env.GOOGLE_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
 		}),
+		DiscordProvider({
+			clientId: process.env.DISCORD_ID,
+			clientSecret: process.env.DISCORD_SECRET,
+		}),
+		TwitterProvider({
+			clientId: process.env.TWITTER_ID,
+			clientSecret: process.env.TWITTER_SECRET,
+		}),
 	],
+	pages: {
+		signIn: "/connexion",
+		// signOut: "/deconnexion",
+	},
 	debug: false,
 };
 
